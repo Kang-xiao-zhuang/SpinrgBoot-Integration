789a6795d2dcbb6cd5db3e1e4ecb9b19fcda383b
ZhuangKang <2247830091@qq.com>
2021-12-10 20:28:13 +0800

    SpringBoot整合Mybatis-Plus

--- SpringBoot-MyBatisPlus/src/main/java/com/zhuang/springbootmybatisplus/controller/StudentController.java
+++ SpringBoot-MyBatisPlus/src/main/java/com/zhuang/springbootmybatisplus/controller/StudentController.java
@@ -1,6 +1,5 @@
 package com.zhuang.springbootmybatisplus.controller;
 
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.zhuang.springbootmybatisplus.entity.Student;
 import com.zhuang.springbootmybatisplus.mapper.StudentMapper;
@@ -28,6 +27,12 @@ public class StudentController {
     @Autowired
     private StudentMapper studentMapper;
 
+    /**
+     * 根据id查询一个学生
+     *
+     * @param id id
+     * @return 学生实体
+     */
     @GetMapping("/findOne/{id}")
     public Student findOne(@PathVariable Integer id) {
         QueryWrapper<Student> studentQueryWrapper = new QueryWrapper<>();
@@ -35,11 +40,23 @@ public class StudentController {
         return studentService.getOne(studentQueryWrapper);
     }
 
+    /**
+     * 增加一个学生
+     *
+     * @param student 学生实体
+     * @return 是否成功
+     */
     @PostMapping("/addStudent")
     public boolean addOne(Student student) {
         return studentService.save(student);
     }
 
+    /**
+     * 根据id删除一个学生
+     *
+     * @param id id
+     * @return 是否成功
+     */
     @DeleteMapping("/deleteStudent/{id}")
     public boolean deleteOne(@PathVariable Integer id) {
         QueryWrapper<Student> studentQueryWrapper = new QueryWrapper<>();
@@ -47,6 +64,12 @@ public class StudentController {
         return studentService.remove(studentQueryWrapper);
     }
 
+    /**
+     * 更新一个学生
+     *
+     * @param student 学生实体
+     * @return 是否成功
+     */
     @PutMapping("/updateStudent")
     public boolean updateOne(Student student) {
         QueryWrapper<Student> studentQueryWrapper = new QueryWrapper<>();
@@ -57,6 +80,11 @@ public class StudentController {
         return studentService.update(newStudent, studentQueryWrapper);
     }
 
+    /**
+     * 获取所有学生信息
+     *
+     * @return List集合
+     */
     @GetMapping("/getAllStudent")
     public List<Student> test() {
         return studentMapper.selectList(null);
