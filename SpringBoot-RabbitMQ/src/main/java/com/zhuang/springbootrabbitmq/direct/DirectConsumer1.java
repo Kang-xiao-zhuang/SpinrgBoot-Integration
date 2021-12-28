0bc74d8f842bd451403580a89b15cc201105a2b9
KangXiaoZhuang <2247830091@qq.com>
2021-12-28 15:31:30 +0800

    直接订阅模式

---
+++ SpringBoot-RabbitMQ/src/main/java/com/zhuang/springbootrabbitmq/direct/DirectConsumer1.java
@@ -0,0 +1,44 @@
+package com.zhuang.springbootrabbitmq.direct;
+
+import com.rabbitmq.client.*;
+import com.zhuang.springbootrabbitmq.utils.RabbitMQUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Classname DirectConsumer1
+ * @Description 直接交换机2
+ * @Date 2021/12/28 14:57
+ * @Author by Zhuang
+ */
+@Slf4j
+public class DirectConsumer1 {
+    public static final String EXCHANGE_NAME = "topic_direct";
+    public static final String QUEUE_NAME = "direct_console";
+    protected static final String[] ROUTING_KEY = {"info", "warning", "error"};
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        try (Channel channel = RabbitMQUtils.getChannel("101.43.21.132", 5672, "admin", "admin", "/");) {
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY[0]);
+            channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY[1]);
+
+            channel.basicConsume(QUEUE_NAME, true, new DeliverCallback() {
+                @Override
+                public void handle(String s, Delivery delivery) throws IOException {
+                    String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                    log.info("接收到的消息" + message);
+                }
+            }, new CancelCallback() {
+                @Override
+                public void handle(String s) throws IOException {
+                    log.warn("接受失败...");
+                }
+            });
+        }
+    }
+}
---
+++ SpringBoot-RabbitMQ/src/main/java/com/zhuang/springbootrabbitmq/direct/DirectProducer.java
@@ -0,0 +1,37 @@
+package com.zhuang.springbootrabbitmq.direct;
+
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.zhuang.springbootrabbitmq.utils.RabbitMQUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Classname DirectProducer
+ * @Description 用一句话描述类的作用
+ * @Date 2021/12/28 15:11
+ * @Author by Zhuang
+ */
+@Slf4j
+public class DirectProducer {
+    public static final String EXCHANGE_NAME = "topic_direct";
+    protected static final String[] ROUTING_KEY = {"info", "warning", "error"};
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        try (Channel channel = RabbitMQUtils.getChannel("101.43.21.132", 5672, "admin", "admin", "/")) {
+            Scanner sc = new Scanner(System.in);
+            log.info("请输入信息");
+            while (sc.hasNext()) {
+                String message = sc.nextLine();
+                channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY[2], null, message.getBytes(StandardCharsets.UTF_8));
+                log.info("生产者发出消息" + message);
+            }
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
