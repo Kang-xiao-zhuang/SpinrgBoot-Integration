61651116a9eca081c417cc775787317fb193a659
KangXiaoZhuang <2247830091@qq.com>
2021-12-28 16:14:40 +0800

    Topic匹配案例

---
+++ SpringBoot-RabbitMQ/src/main/java/com/zhuang/springbootrabbitmq/all/AllConsumer1.java
@@ -0,0 +1,50 @@
+package com.zhuang.springbootrabbitmq.all;
+
+import com.rabbitmq.client.CancelCallback;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.Delivery;
+import com.zhuang.springbootrabbitmq.utils.RabbitMQUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Classname AllConsumer1
+ * @Description 所有交换机
+ * @Date 2021/12/28 15:35
+ * @Author by Zhuang
+ */
+@Slf4j
+public class AllConsumer1 {
+    public static final String EXCHANGE_NAME = "topic_logs";
+    protected static final String[] QUEUE_NAME = {"Q1", "Q2"};
+
+    public static void main(String[] args) {
+        try (Channel channel = RabbitMQUtils.getChannel("101.43.21.132", 5672, "admin", "admin", "/");) {
+            String type="topic";
+            channel.exchangeDeclare(EXCHANGE_NAME,type);
+            channel.queueDeclare(QUEUE_NAME[0], false, false, false, null);
+            channel.queueBind(QUEUE_NAME[0], EXCHANGE_NAME, "*.*.rabbit");
+            channel.queueBind(QUEUE_NAME[0], EXCHANGE_NAME, "lazy.#");
+
+            channel.basicConsume(QUEUE_NAME[0], true, new DeliverCallback() {
+                @Override
+                public void handle(String s, Delivery delivery) throws IOException {
+                    String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+                    log.info("接收到的消息" + message);
+                    log.info("队列名->" + QUEUE_NAME[0] + "绑定键->" + delivery.getEnvelope().getRoutingKey());
+                }
+            }, new CancelCallback() {
+                @Override
+                public void handle(String s) throws IOException {
+                    log.warn("接受失败...");
+                }
+            });
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ SpringBoot-RabbitMQ/src/main/java/com/zhuang/springbootrabbitmq/all/AllProducer.java
@@ -0,0 +1,42 @@
+package com.zhuang.springbootrabbitmq.all;
+
+import com.rabbitmq.client.Channel;
+import com.zhuang.springbootrabbitmq.utils.RabbitMQUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Classname AllProducer
+ * @Description 用一句话描述类的作用
+ * @Date 2021/12/28 15:49
+ * @Author by Zhuang
+ */
+@Slf4j
+public class AllProducer {
+    public static final String EXCHANGE_NAME = "topic_logs";
+    protected static final String[] QUEUE_NAME = {"Q1", "Q2"};
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        try (Channel channel = RabbitMQUtils.getChannel("101.43.21.132", 5672, "admin", "admin", "/");) {
+            HashMap<String, String> bindingKeyMap = new HashMap<>(20);
+            bindingKeyMap.put("quick.orange.rabbit", "被队列 Q1Q2 接收到");
+            bindingKeyMap.put("lazy.orange.elephant", "被队列 Q1Q2 接收到");
+            bindingKeyMap.put("quick.orange.fox", "被队列 Q1 接收到");
+            bindingKeyMap.put("lazy.brown.fox", "被队列 Q2 接收到");
+            bindingKeyMap.put("lazy.pink.rabbit", "虽然满足两个绑定但只被队列 Q2 接收一次");
+            bindingKeyMap.put("quick.brown.fox", "不匹配任何绑定不会被任何队列接收到会被丢弃");
+            bindingKeyMap.put("quick.orange.male.rabbit", "是四个单词不匹配任何绑定会被丢弃");
+            bindingKeyMap.put("lazy.orange.male.rabbit", "是四个单词但匹配 Q2");
+            for (Map.Entry<String, String> bindingKeyEntry : bindingKeyMap.entrySet()) {
+                String key = bindingKeyEntry.getKey();
+                String value = bindingKeyEntry.getValue();
+                channel.basicPublish(EXCHANGE_NAME, key, null, value.getBytes(StandardCharsets.UTF_8));
+            }
+        }
+    }
+}
