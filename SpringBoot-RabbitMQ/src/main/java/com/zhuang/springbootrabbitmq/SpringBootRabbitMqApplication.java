6339334cf43fc973d7ffe9354554a5498db83587
KangXiaoZhuang <2247830091@qq.com>
2021-12-29 17:53:11 +0800

    修改配置文件

--- SpringBoot-RabbitMQ/src/main/java/com/zhuang/springbootrabbitmq/SpringBootRabbitMqApplication.java
+++ SpringBoot-RabbitMQ/src/main/java/com/zhuang/springbootrabbitmq/SpringBootRabbitMqApplication.java
@@ -2,8 +2,10 @@ package com.zhuang.springbootrabbitmq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootRabbitMqApplication {
 
     public static void main(String[] args) {
