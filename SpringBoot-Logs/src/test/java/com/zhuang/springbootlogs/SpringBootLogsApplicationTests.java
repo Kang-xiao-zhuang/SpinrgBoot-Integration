3aedaf9516985f4f8a7cdaed67771b1ad0e54692
KangXiaoZhuang <2247830091@qq.com>
2022-01-20 12:00:13 +0800

    更新代码

--- SpringBoot-Logs/src/test/java/com/zhuang/springbootlogs/SpringBootLogsApplicationTests.java
+++ SpringBoot-Logs/src/test/java/com/zhuang/springbootlogs/SpringBootLogsApplicationTests.java
@@ -1,7 +1,9 @@
 package com.zhuang.springbootlogs;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootTest
 class SpringBootLogsApplicationTests {
@@ -10,4 +12,16 @@ class SpringBootLogsApplicationTests {
     void contextLoads() {
     }
 
+    //记录器
+    public static final Logger LOGGER = LoggerFactory.getLogger(SpringBootLogsApplicationTests.class);
+
+    @Test
+    void test() {
+    // 打印日志信息
+        LOGGER.error("error");
+        LOGGER.warn("warn");
+        LOGGER.info("info"); // 默认日志级别
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");
+    }
 }
